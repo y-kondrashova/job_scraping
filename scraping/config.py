93584b4276de6af7217b4d785f4720650ff13486
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-12-20 11:23:36 +0200

    Add technology list to project

---
+++ scraping/config.py
@@ -0,0 +1,15 @@
+TECHNOLOGIES = [
+    "Python", "Django", "Web API", "Flask", "FastAPI", "Pyramid", "Tornado",
+    "Bottle", "AWS", "RoboDK", "API", "Azure", "Google Cloud Platform",
+    "Docker", "Kubernetes", "Terraform", "Ansible", "Celery", "Redis",
+    "RabbitMQ", "Apache Kafka", "SQL", "PostgreSQL", "MySQL", "SQLite",
+    "MongoDB", "Excel", "Elasticsearch", "SQLAlchemy", "BigQuery", "Peewee",
+    "Jenkins", "GitLab CI", "CircleCI", "Travis CI", "Sentry", "New Relic",
+    "Grafana", "Prometheus", "pytest", "unittest", "coverage.py", "tox",
+    "requests", "httpx", "aiohttp", "BeautifulSoup", "Selenium", "Scrapy",
+    "Pytest-Django", "Flask-RESTful", "REST API", "DRF", "Pydantic",
+    "Marshmallow", "OpenCV", "TensorFlow", "PyTorch", "scikit-learn", "Keras",
+    "NLTK", "spaCy", "Jupyter", "Matplotlib", "Seaborn", "Plotly", "Dash",
+    "Pygame", "Pandas", "NumPy", "SciPy", "SymPy", "ML", "CI/CD", "GIT",
+    "GitHub", "JavaScript", "React",
+]
